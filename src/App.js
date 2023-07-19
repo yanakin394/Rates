6170e8e47ccc044a913ff8968f9a60d4858ca957
yanakin394 <yana.germatskaya@gmail.com>
2023-07-19 14:40:56 +0300

    rates components added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './style/App.css';
 
 function App() {
   return (
-    <div className="ratesContainer">
-      <Rates/>
-      <Rates/>
-      <Rates/>
-      <Rates/>
+    <div className="App">
+      <Rates name="Безлимитный 300" price="300" speed="10"></Rates>
+      <Rates name="Безлимитный 450" price="450" speed="50"></Rates>
+      <Rates name="Безлимитный 550" price="550" speed="100"></Rates>
+      <Rates name="Безлимитный 1000" price="1000" speed="200"></Rates>
     </div>
   );
 }
--- src/components/Rates.jsx
+++ src/components/Rates.jsx
@@ -1,11 +1,15 @@
-/* import '../style/Rates.scss'; */
+import '../style/Rates.css';
 
-function Rates() {
+function Rates(props) {
     return ( 
             <div className="unlimited">
-                <div className="rate_title">Безлимитный 300</div>
-                <div className="rate_price">руб. 300/мес.</div>
-                <div className="rate_speed">до 10Мбит/сек.</div>
+                <div className="rate_title">{props.name}</div>
+                <div className="rate_price">
+                    <p className='rate_currency'>руб.</p>
+                    <p className='rate_number'>{props.price}</p>
+                    <p className='rate_month'>/мес.</p>  
+                </div>
+                <div className="rate_speed">до {props.speed} Мбит/сек.</div>
                 <div className="rate_traffic">Обьем включенного траффика не ограничен</div>
             </div>
     );
