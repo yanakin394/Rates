9fa5f0bfe870834a0db663bff1006300fdacb2e2
yanakin394 <yana.germatskaya@gmail.com>
2023-07-19 12:05:33 +0300

    rates

---
+++ src/components/Rates.jsx
@@ -0,0 +1,14 @@
+/* import '../style/Rates.scss'; */
+
+function Rates() {
+    return ( 
+            <div className="unlimited">
+                <div className="rate_title">Безлимитный 300</div>
+                <div className="rate_price">руб. 300/мес.</div>
+                <div className="rate_speed">до 10Мбит/сек.</div>
+                <div className="rate_traffic">Обьем включенного траффика не ограничен</div>
+            </div>
+    );
+}
+
+export default Rates;
